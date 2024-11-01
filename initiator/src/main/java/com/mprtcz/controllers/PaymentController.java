6091ef49e1ced3152b6726ca27fcb46381b8093d
michal <michalpartacz@gmail.com>
2024-11-01 07:32:53 +0100

    Small refactor.

--- initiator/src/main/java/com/mprtcz/controllers/PaymentController.java
+++ initiator/src/main/java/com/mprtcz/controllers/PaymentController.java
@@ -1,17 +1,19 @@
 package com.mprtcz.controllers;
 
 import com.mprtcz.controllers.dto.PaymentRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class PaymentController {
 
     @PostMapping("/v1/payment/initiate")
     public ResponseEntity<Void> initiatePayment(@RequestBody PaymentRequest paymentRequest) {
-        System.out.println("paymentRequest = " + paymentRequest);
+        log.info("paymentRequest = {}", paymentRequest);
         return ResponseEntity.ok().build();
     }
 }
